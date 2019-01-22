9c26a3dc15a1fe5f19af6f4562191172751d8d5f
domingobishop <domingobishop@gmail.com>
2019-01-22 10:52:04 +0000

    Setup

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
-# wp-docker
+# tna-wp-dev
 
-### Enviroment variables
+A Docker WordPress development environment for TNA.
 
-Create an “.env” file in the same directory as the docker-compose file. (Do not commit this file to Github)
-
-Add the enviroment variables to this file:
-
-`DB_PASS=SelectAnotherPassword`
-
-### Make build.sh executable:
+### Make build.sh executable
 
 `chmod +x ./build.sh`
 
-### Run build:
+### Run build
 
-`./build.sh [URL] "[SITE NAME]" [ADMIN USER] [ADIM PASSOWRD] [ADMIN EMAIL]`
+`./build.sh [ADMIN EMAIL]`
--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ docker-compose up -d
 tput setaf 6; echo "Wait for database..."; tput sgr0
 sleep 30
 
-WP_URL=$1
-WP_TITLE=$2
-WP_ADMIN_USER=$3
-WP_ADMIN_PASS=$4
-WP_ADMIN_EMAIL=$5
+WP_URL=localhost:8080
+WP_TITLE="TNA WordPress Development"
+WP_ADMIN_USER=admin
+WP_ADMIN_PASS=admin
+WP_ADMIN_EMAIL=$1
 wp="docker-compose run --rm wp-cli"
 
 # Install and configure Wordpress
@@ -22,10 +22,4 @@ $wp widget reset --all
 
 # Install plugins
 tput setaf 6; echo "Installing plugins..."; tput sgr0
-$wp plugin install https://downloads.wordpress.org/plugin/co-authors-plus.3.3.1.zip --activate
 $wp plugin install https://downloads.wordpress.org/plugin/wordpress-seo.9.4.zip --activate
-
-# Install themes
-tput setaf 6; echo "Installing themes..."; tput sgr0
-$wp theme install https://github.com/nationalarchives/tna-base/archive/master.zip --force
-$wp theme install https://github.com/nationalarchives/tna-child-blog/archive/master.zip --activate --force
