dbb96eb61e2c4dd16ccf442ac5dccd068c188703
domingobishop <domingobishop@gmail.com>
2019-01-31 15:07:26 +0000

    port var for wp URL install

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+source .env
+
 docker-compose up -d
 
 tput setaf 6; echo "Wait for database..."; tput sgr0
-sleep 30
+sleep 20
 
-WP_URL=localhost:8080
+WP_URL=localhost:${APP_PORT}
 WP_TITLE="TNA WordPress Development"
 WP_ADMIN_USER=admin
 WP_ADMIN_PASS=admin
@@ -13,7 +15,11 @@ WP_ADMIN_EMAIL=$1
 wp="docker-compose run --rm wp-cli"
 
 # Install and configure Wordpress
-tput setaf 6; echo "Installing Wordpress..."; tput sgr0
+tput setaf 6
+echo "Installing Wordpress..."
+echo "URL: localhost:"${APP_PORT}
+echo "Site name: "${WP_TITLE}
+tput sgr0
 $wp core install --url="${WP_URL}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN_USER}" --admin_password="${WP_ADMIN_PASS}" --admin_email="${WP_ADMIN_EMAIL}"
 tput setaf 6; echo "Configuring Wordpress..."; tput sgr0
 $wp config set WP_DEBUG true
